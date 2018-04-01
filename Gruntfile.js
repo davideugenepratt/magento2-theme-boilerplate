8bcb6ba2048e4e32261b0165925225162f77c3ad
= <david@davideugenepratt.com>
2018-04-01 11:26:42 -0700

    Adds registration.php stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,12 @@ module.exports = function(grunt) {
     grunt.initConfig({
         exec: {
             themeGenerateXml: 'sed -i "s#<title>New theme</title>#<title>' + composer.description + '</title>#g" theme.xml && sed -i "s#<parent>Magento/blank</parent>#<parent>' + composer[ "parent-theme" ] + '</parent>#g" theme.xml',
+            themeGenerateRegistration: 'sed -i "s#frontend/<Vendor>/<theme>#frontend/' + composer.name + '#g" registration.php',
             themeDirectoryCheck: 'if [ ! -d "' + themeDirectory + '" ]; then mkdir -p ' + themeDirectory + ' && echo "Created theme directory"; fi',
             clearThemeDirectory : 'rm -rf ' + themeDirectory + '/*',
             themeCopyFiles: 'rsync -av --progress /vagrant/. ' + themeDirectory + ' --exclude vendor --exclude node_modules --exclude vagrant --exclude .git --exclude .idea'
         }
     });
-    grunt.registerTask( 'theme:generate' , [ 'exec:themeGenerateXml' ] );
+    grunt.registerTask( 'theme:generate' , [ 'exec:themeGenerateXml' , 'exec:themeGenerateRegistration' ] );
     grunt.registerTask( 'theme:copy' , [ 'exec:themeDirectoryCheck', 'exec:clearThemeDirectory', 'exec:themeCopyFiles' ] );
 };
