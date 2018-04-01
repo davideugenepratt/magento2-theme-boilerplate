575e582cb7229f57de2e9351be038932b746cf07
= <david@davideugenepratt.com>
2018-04-01 11:12:27 -0700

    Adds some theme generation stuff

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+    var composer = grunt.file.readJSON( 'composer.json' );
+    var themeDirectory = '/vagrant/vendor/' + composer['demo-site'] + '/magento2/app/design/frontend/' + composer.name;
+    grunt.initConfig({
+        exec: {
+            themeGenerateXml: 'sed -i "s#<title>New theme</title>#<title>' + composer.description + '</title>#g" theme.xml && sed -i "s#<parent>Magento/blank</parent>#<parent>' + composer[ "parent-theme" ] + '</parent>#g" theme.xml',
+            themeDirectoryCheck: 'if [ ! -d "' + themeDirectory + '" ]; then mkdir -p ' + themeDirectory + ' && echo "Created theme directory"; fi',
+            clearThemeDirectory : 'rm -rf ' + themeDirectory + '/*',
+            themeCopyFiles: 'rsync -av --progress /vagrant/. ' + themeDirectory + ' --exclude vendor --exclude node_modules --exclude vagrant --exclude .git --exclude .idea'
+        }
+    });
+    grunt.registerTask( 'theme:generate' , [ 'exec:themeGenerateXml' ] );
+    grunt.registerTask( 'theme:copy' , [ 'exec:themeDirectoryCheck', 'exec:clearThemeDirectory', 'exec:themeCopyFiles' ] );
+};
